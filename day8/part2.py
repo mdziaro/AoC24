3b72007b0f610f56a63e23baa36a9ae52c25597c
mdziaro <mdziaro@student.agh.edu.pl>
2024-12-08 13:12:03 +0100

    Day 8

---
+++ day8/part2.py
@@ -0,0 +1,41 @@
+file = open("input.txt", "r")
+data = file.readlines()
+
+for i in range(len(data)):
+    if data[i][-1] == "\n":
+        data[i] = data[i][:-1]
+
+letters_to_places = {}
+
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        if data[i][j] == ".":
+            pass
+        elif letters_to_places.get(data[i][j]) == None:
+            letters_to_places[data[i][j]] = [(i, j)]
+        else:
+            letters_to_places[data[i][j]].append((i,j))
+
+locations = set()
+for key in letters_to_places:
+    lista = letters_to_places[key]
+    print(lista)
+    for i in range(len(lista)-1):
+        for j in range(i+1, len(lista)):
+            for k in range(50):
+                diff = (lista[j][0]-lista[i][0], lista[j][1]-lista[i][1])
+                locations.add((lista[i][0]-k*diff[0], lista[i][1]-k*diff[1]))
+                locations.add((lista[j][0]+k*diff[0], lista[j][1]+k*diff[1]))
+
+# remove out of bounds
+height = len(data)-1
+width = len(data[1])-1
+right_locations = []
+locations = list(locations)
+for i in range(len(locations)):
+    if locations[i][0] < 0 or locations[i][0] > height or locations[i][1] < 0 or locations[i][1] > width:
+        pass
+    else:
+        right_locations.append(locations[i])
+
+print(len(right_locations))
