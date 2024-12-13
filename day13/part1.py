651abed9e10baec3f5fd9f1f3d418b45fe03456f
mdziaro <mdziaro@student.agh.edu.pl>
2024-12-13 11:59:18 +0100

    Day 13

---
+++ day13/part1.py
@@ -0,0 +1,41 @@
+import re
+
+file = open("input.txt")
+data = file.readlines()
+file.close()
+
+# Pattern to extract all numbers
+pattern = r"[-+]?\d+"
+
+# Store results
+results = []
+
+for line in data:
+    # Extract numbers from each line
+    numbers = re.findall(pattern, line)
+    if numbers:
+        results.append([int(num) for num in numbers])
+
+print(results)
+equations = []
+
+for i in range(0,len(results),3):
+    equations.append([results[i][0], results[i+1][0], results[i+2][0]])
+    equations.append([results[i][1], results[i+1][1], results[i+2][1]])
+
+sum = 0
+
+for i in range(0, len(equations), 2):
+    a1, b1, c1 = equations[i]
+    a2, b2, c2 = equations[i+1]
+
+    det = a1 * b2 - a2 * b1
+    det_x = c1 * b2 - c2 * b1
+    det_y = a1 * c2 - a2 * c1
+
+    x = det_x / det
+    y = det_y / det
+
+    if (x - x // 1 == 0) and (y - y // 1 == 0):
+        sum += x*3 + y
+print(sum)
