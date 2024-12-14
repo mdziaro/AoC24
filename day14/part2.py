28cd215fd872f9dfeaafd0c7ea5e82dbdb400260
mdziaro <mdziaro@student.agh.edu.pl>
2024-12-14 14:57:22 +0100

    Day 14

--- day14/part2.py
+++ day14/part2.py
@@ -1,6 +1,7 @@
 import re
 from PIL import Image, ImageDraw
-for seconds in range(6660,6900):
+for seconds in range(0,10000):
+    
     file = open("input.txt")
     data = file.readlines()
     file.close()
@@ -9,9 +10,10 @@ for seconds in range(6660,6900):
         data[i] = re.findall(r"(-?\d+,-?\d+)", data[i]) 
         data[i][0] = list(map(int, data[i][0].split(",")))
         data[i][1] = list(map(int, data[i][1].split(",")))
+
     width = 101
     height = 103
-    views = []
+
     fin_pos = []
 
     for i in range(len(data)):
@@ -21,15 +23,28 @@ for seconds in range(6660,6900):
         pos[1] = (pos[1] + v[1] * seconds) % height
         fin_pos.append((pos[0], pos[1]))
 
-    view = [["." for _ in range(width+2)] for _ in range(height+2)]
-    for i,j in fin_pos:
-        view[i][j] = "#"
-    img = Image.new("RGB", (width + 2, height + 2), color=(255, 255, 255))
-    draw = ImageDraw.Draw(img)
+    results = [0, 0, 0, 0]
 
-    for i in range(height + 2):
-        for j in range(width + 2):
-            if view[i][j] == "#":
-                draw.point((j, i), fill=(0, 0, 0))
+    for i in range(len(fin_pos)):
+        if fin_pos[i][0] < width // 2 and fin_pos[i][1] < height // 2:
+            results[0] += 1
+        elif fin_pos[i][0] > width // 2 and fin_pos[i][1] < height // 2:
+            results[1] += 1
+        elif fin_pos[i][0] < width // 2 and fin_pos[i][1] > height // 2:
+            results[2] += 1
+        elif fin_pos[i][0] > width // 2 and fin_pos[i][1] > height // 2:
+            results[3] += 1
 
-    img.save(f"output_second_{seconds}.png")
+    if max(results) > 200:
+        view = [["." for _ in range(width+2)] for _ in range(height+2)]
+        for i,j in fin_pos:
+            view[i][j] = "#"
+        img = Image.new("RGB", (width + 2, height + 2), color=(255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        for i in range(height + 2):
+            for j in range(width + 2):
+                if view[i][j] == "#":
+                    draw.point((j, i), fill=(0, 0, 0))
+
+        img.save(f"output_second_{seconds}.png")
